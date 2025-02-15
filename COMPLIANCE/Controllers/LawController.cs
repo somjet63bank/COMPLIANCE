79d6c5cb19bb45eae53ef04cdbce54a22f13f735
Pakornpong hiranjaraspiwat <85547494+methawinsss@users.noreply.github.com>
2025-02-15 13:46:11 +0700

    ui home and Law

---
+++ COMPLIANCE/Controllers/LawController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMPLIANCE.Controllers
+{
+    public class LawController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Edit()
+        {
+            return View();
+        }
+
+    }
+}
